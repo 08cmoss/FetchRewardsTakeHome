a99bcaef5dec46b43c8f44755e7998323bbff989
Cameron Moss <08cmoss@gmail.com>
2025-02-03 09:13:51 -0500

    Initial Commit

---
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Cameron Moss on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
